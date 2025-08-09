f93d8372a8840b9cf323d735490c3f83e2a061c1
Perma <158625913+Permadim@users.noreply.github.com>
2025-08-09 18:38:46 -0300

    Update calculator.c
    I made a small yet necessary upgrade to the previous one that only allowed sum, but now you can chose what type of operation you want to make
    Next i will try to allow float numbers and make it prettier (somehow?)

--- calculator_project/calculator.c
+++ calculator_project/calculator.c
@@ -2,13 +2,36 @@
 
 int main()
 {
-    int a, b;
-    printf("Bem vindo ao somador em C\n");
-    printf("O primeiro numero:\n");
-    scanf("%d",&a);
-    printf("O segundo numero:\n");
-    scanf("%d",&b);
-    int c = a + b;
-    printf("A soma de %d e %d sera: %d\n",a, b, c);
+    int operation, num1, num2, result;
+    printf("Welcome to this calculator made in C!!\n");
+    printf("1-Sum\n2-Subtraction\n3-Multiplication\n4-Division\n");
+    scanf("%d",&operation);
+    printf("Whats the first number?\n");
+    scanf("%d", &num1);
+    printf("Whats the second number?\n");
+    scanf("%d", &num2);
+    switch(operation)
+    {
+        case 1:
+            result = num1 +num2;break;
+        case 2:
+            result = num1 - num2;break;
+            
+        case 3:
+            result = num1 * num2;break;
+        case 4:
+            if (num2 == 0)
+            {
+                printf("you cant defide by 0\n");
+                return 1;
+            }
+            result = num1 / num2; break;
+        default:
+            printf("INvalid option\n"); return 1;
+        
+
+    }
+    printf("The equation equals to %d\n", result);
+    printf("operation is equal to %d\n",operation);
     return 0;
 }
